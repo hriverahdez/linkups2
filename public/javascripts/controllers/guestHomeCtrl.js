a3a154c5433c8d0cd0d9b4dc288d01f02eb3f0f9
Helian Rivera Hernandez <hriverahdez2011@gmail.com>
2016-12-11 13:28:22 -0500

    Working on guestHome

--- public/javascripts/controllers/guestHomeCtrl.js
+++ public/javascripts/controllers/guestHomeCtrl.js
@@ -1,5 +1,30 @@
 angular.module('linkups2').controller('guestHomeCtrl', [
 	'$scope',
-	function($scope) {
+	'utilityService',
+	function($scope, utilityService) {
+		$scope.typeIconClasses = utilityService.getInstTypeIcons("fa-2x fa-fw");
+		$scope.internetStatusIcons = utilityService.getHasInternetIcons("fa-1x");
+		$scope.reqStatusIcons = {
+			true: 'fa fa-check',
+			false: 'fa fa-close'
+		};
+		$scope.requests = [
+			{
+				name: 'Policlinico Fulano de tal',
+				type: 'policlinico',
+				location:  'Pinar del Rio',
+				has_internet: true,
+				bandwidth: '256',
+				approved: true
+			},
+			{
+				name: 'Policlinico Fulano de tal',
+				type: 'policlinico',
+				location:  'San Juan y Martinez',
+				has_internet: false,
+				bandwidth: '256',
+				approved: false
+			}
+		];
 	}
 ]);
